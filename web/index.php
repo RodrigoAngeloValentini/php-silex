7fe816dc9c5b81172fe0a7a620051923939600bb
Rodrigo Angelo Valentini <rodrigoangelovalentini@hotmail.com>
2016-04-25 13:17:10 -0300

    middlewares

--- web/index.php
+++ web/index.php
@@ -13,7 +13,41 @@ $app['debug'] = true;
 //    return (int) $id;
 //});
 
-$app->get('/{nome}',function($nome){
+$before = function(){
+    echo "Executou antes do callback";
+};
+
+$after = function(Request $request, Response $response) use ($app){
+    echo "Rodou depois do callback";
+};
+
+$app->get('/nome/{nome}',function($nome){
     return new Response("Ola mundo:{$nome}",200);
-})->value('nome','Rodrigo');
+})
+    ->value('nome','Rodrigo')
+    ->bind('rota_rodrigo')
+    ->before($before)
+    ->after($after);
+
+$app->get('/json',function() use($app){
+    $array = array('nome'=>'rodrigo');
+    $erro = array('message'=>'Ero ao processar');
+    return $app->json($erro,404);
+});
+
+$app->before(function(Request $request){
+    //echo "Rodou antes";
+}, Silex\Application::EARLY_EVENT);
+
+//Executado antes do response para o browser
+$app->after(function(Request $request, Response $response){
+    //echo "Rodou depois";
+});
+
+//Executado depois do response para o browser
+$app->finish(function(Request $request, Response $response){
+    //echo "Rodou depois";
+});
+
+
 $app->run();
