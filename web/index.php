caf527267a38cbb69afd6c0cfd56cdcdf8ea6ec5
Rodrigo Angelo Valentini <rodrigoangelovalentini@hotmail.com>
2016-04-29 15:46:06 -0300

    twig

--- web/index.php
+++ web/index.php
@@ -7,33 +7,19 @@ use Symfony\Component\HttpFoundation\Response;
 $app = new Silex\Application();
 $app['debug'] = true;
 
-//$app['res'] = function(){
-//  return new Response('OI');
-//};
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+   'twig.path' => __DIR__.'/../views',
+));
 
-$app['res'] = $app->share(function(){
-    return new Response('OI');
-});
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
-$res1 = $app['res'];
-$res2 = $app['res'];
+$app->get('/ola/{nome}',function($nome) use ($app){
+    return $app['twig']->render('ola.twig',array('nome'=>$nome));
+})->bind('ola');
 
-if($res1 === $res2){
-    echo "São iguais";
-}else{
-    echo "São diferentes";
-}
+$app->get('/link/{nome}',function($nome) use ($app){
+    return $app['twig']->render('link.twig',array('nome'=>$nome));
+})->bind('link');
 
-//$app['pdo'] = function(){
-//    return new PDO('dsn','usuario','senha');
-//};
-//$app['pessoa'] = function() use($app){
-//    $pdo = $app['pdo'];
-//    return new Pessoa($pdo);
-//};
-//$pessoa = $app['pessoa'];
-
-$app->mount("/enquete",include 'enquete.php');
-$app->mount("/forum",include 'forum.php');
 
 $app->run();
